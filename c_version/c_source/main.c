32c0a371b60b2ffcf2a48eb4bd6043ed1b69d9c2
Ny Hasina <djazejhasi@gmail.com>
2025-04-19 21:45:09 +0300

    Prevent Segfault by counting the parsed notifications number

--- c_version/c_source/main.c
+++ c_version/c_source/main.c
@@ -6,6 +6,7 @@ int	main(int argc, char *argv[], char *envp[])
 	char	__TEMPLATE_BUFFER[__INTERNAL_BUFF__] = {0};
 	s_notif	__NOTIFICATION_OBJECTS__[__MAX_NOTIF__] = {0};
 	size_t	len;
+	size_t	__PARSED_DATA;
 
 	(void)argc;
 	(void)argv;
@@ -13,22 +14,22 @@ int	main(int argc, char *argv[], char *envp[])
 
 	fetch_bus_data(__BUFFER, envp);
 	len = strlen(__BUFFER);
-	if (len < 13)
-		output("No notifications\n");
+	if (len < 31)
+		output("(label :class \"label\" :text \"No Notifications\")\n");
 	else
 	{
 		char	*__PTR_BUFFER = __BUFFER;
 		e_err	__CODE_ERROR;
 
 		__PTR_BUFFER += 12;
-		parse_data(__PTR_BUFFER, __NOTIFICATION_OBJECTS__);
+		parse_data(__PTR_BUFFER, __NOTIFICATION_OBJECTS__, &__PARSED_DATA);
 		__CODE_ERROR = fetch_template(__TEMPLATE_BUFFER);
 		if (__CODE_ERROR == __OK__)
 		{
 			size_t	iteration = 0;
 
 			output("(box :class \"box\" :orientation \"v\" :space-evenly false\n");
-			while (__NOTIFICATION_OBJECTS__[iteration].__VALID_SLOT == true)
+			while (iteration < __PARSED_DATA)
 			{
 				output_notifications(__TEMPLATE_BUFFER, &__NOTIFICATION_OBJECTS__[iteration]);
 				iteration++;
